1931b0a9472765eeed71f2cbeea57a80448aee5a
Crystal Pederson <crystalpederson88@gmail.com>
2022-07-13 10:40:51 -0700

    Part 6- get data from server

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -20,7 +20,7 @@ export class DashboardComponent implements OnInit {
   //gets the top 5 heroes asynchronously
   getHeroes(): void{
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1,5))
+      .subscribe(heroes => this.heroes = heroes.slice(0,4))
   }
 
 }
